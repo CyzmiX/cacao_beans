6eaf240eb9f34b5614ca56b5398c8794f0636189
CyzmiX <98268356+CyzmiX@users.noreply.github.com>
2024-02-06 19:23:12 +0000

    dd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,12 @@
 </script>
 
 <slot />
+
+<style>
+:global(body) {
+
+  max-width: 500px;
+  margin: auto;
+
+}
+    </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1 class="text-2xl text-center">GOT 100 OF THESE?</h1>
+
+<img alt="beans" src="cacao.webp"/>
+<h1 class="text-2xl text-center">BUY A SLAVE NOW!</h1>
+<h3 class="text-xm text-center">use code "slavery" for 15% off!!</h3>
+
+<button class=" inline-flex items-center justify-center ml-20 p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white  focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
+    BUY NOW!!
+    </span>
+    </button>
